ae97dd3db725a094350a4745b0f665b4a53abc2d
9Taha <9taha9ahmed@gmail.com>
2022-03-10 12:56:57 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,7 +13,7 @@ int main(void)
 
 {
 
-	    putf("\"Programming is like building a multilingual puzzle");
+	    puts("\"Programming is like building a multilingual puzzle");
 
 	        return (0);
 
