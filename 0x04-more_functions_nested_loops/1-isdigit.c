787aa7a4154293f06c918d98e275d07190d25fec
9Taha <9taha9ahmed@gmail.com>
2022-03-18 07:58:45 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,27 +8,19 @@
  *   * @c: variable
  *
  *     * Return: Always 0.
- *      
+ *
  */
 
-int _isdigit(int c)
+	int _isdigit(int c)
 
 {
 
 	if (c >= 48 && c <= 57)
-
 	{
-
 	return (1);
-
 	}
-
 	else
-
 	{
-
 	return (0);
-
 	}
-
 }
