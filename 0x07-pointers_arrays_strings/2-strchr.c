bbcf4e37be61b42bdf24f6a6e1fa929121a0ad33
9Taha <9taha9ahmed@gmail.com>
2022-03-29 05:24:46 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,7 @@
  *
  *     * @c: the character we're looking for
  *
- *       * Return: a pointer to the first occurance of the character @c in the string
+ *    *Return:a pointer to the first occurance of the character @c in the string
  *
  *        * @s. Return NULL if the character isn't found
  *
@@ -20,14 +20,23 @@ char *_strchr(char *s, char c)
 {
 
 	while (*s)
+
 	{
+	
 	if (*s != c)
+
 	s++;
+
 	else
+
 	return (s);
+
 	}
+
 	if (c == '\0')
+
 	return (s);
+
 	return (NULL);
 
 }
